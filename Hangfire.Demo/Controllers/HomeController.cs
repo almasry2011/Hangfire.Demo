244be1abf176a65587b61c2acfb6b0b17deab778
Amr Mohamed <amr.mohamed0eg@outlook.com>
2020-07-16 12:55:00 +0200

    Hang Fire Demo

---
+++ Hangfire.Demo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hangfire.Demo.Models;
+
+namespace Hangfire.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ////Fire-and-forget
+         //   BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget Job Executed"));
+
+            ////Delayed job
+         //  BackgroundJob.Schedule(() => Console.WriteLine("Delayed job executed"), TimeSpan.FromMinutes(1));
+          
+           RecurringJob.AddOrUpdate(() => Console.WriteLine("Minutely Job executed"), Cron.Minutely);
+
+            //var id = BackgroundJob.Enqueue(() => Console.WriteLine("Hello, "));
+            // BackgroundJob.ContinueWith(id, () => Console.WriteLine("world!"));
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Hangfire.Demo/Models/DbModels.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangfire.Demo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        ICollection<Employee> Employees { get; set; }
+    }
+
+
+    public class AppdbContext:DbContext
+    {
+        public AppdbContext(DbContextOptions<AppdbContext> options) : base(options)
+        { }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+
+}
